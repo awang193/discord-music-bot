59c93e83182d57b513e961a82898b9ed75608927
awang193 <awang193@gmail.com>
2020-01-03 15:37:21 -0800

    Began implementing play functionality, added data persistence

--- commands/playback/play.js
+++ commands/playback/play.js
@@ -1,7 +1,28 @@
-module.exports = {
-    name: '',
-    category: '',
-    run: async (client, message, args) => {
+const ytdl = require('ytdl-core');
 
+module.exports = {
+    name: 'play',
+    category: 'playback',
+    run: async (client, message, args) => {
+        const server_name = message.guild.name;
+        
+        args.forEach(url => {
+            const lower_url = url.toLowerCase()
+
+            if (!lower_url.startsWith("https://")) return;
+            
+            if (lower_url.includes("youtube") || lower_url.includes("youtu.be")) {
+                ytdl.getInfo(url)
+                    .then(info => {
+                        client.server_data[server_name]["queue"].push({
+                            "name": info[1].title,
+                            "url": info[1].video_url
+                        });  
+                    })
+                    .catch(err => console.log(`<YTDL-ERROR> ${err}`)); 
+            }
+        });
+
+        console.log(`<DEBUG> Current queue: ${JSON.stringify(client.server_data[server_name]["queue"])}`);
     }
 };
--- index.js
+++ index.js
@@ -1,47 +1,99 @@
-require('dotenv').config();
-const fs = require('fs');
-const Discord = require('discord.js');
+require("dotenv").config();
+const fs = require("fs");
+const jsonfile = require("jsonfile");
+const { Client, Collection } = require("discord.js");
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
 
-// Invoke handlers
-fs.readdirSync('./handlers/').forEach(handler => {
-    require('./handlers/' + handler)(client);
+
+// Backup data once every 60s
+async function backupLoop() {
+    jsonfile.writeFile('./data/servers.json', server_data)
+        .catch(err => console.log("<ERROR> Server data write failed! ⛔"));
+
+    jsonfile.writeFile('./data/users.json', user_data)
+        .catch(err => console.log("<ERROR> User data write failed! ⛔")); 
+
+    jsonfile.writeFile('./data/playlists.json', playlists)
+        .catch(err => console.log("<ERROR> Playlist data write failed! ⛔"));
+}
+
+//setInterval(backupLoop, 5000);
+
+
+
+// Client settings and data
+const client = new Client();
+const discord_token = process.env.DISCORD_TOKEN;
+
+client.commands = new Collection();
+client.server_data = jsonfile.readFileSync("./data/servers.json");
+client.user_data = jsonfile.readFileSync("./data/users.json");
+client.playlists = jsonfile.readFileSync("./data/playlists.json");
+
+fs.readdirSync("./handlers/").forEach(handler => {
+    require("./handlers/" + handler)(client);
 });
 
-// Log in
-const discord_token = process.env.DISCORD_TOKEN;
-client.login(discord_token);
+//client.commands.forEach(cmd => console.log(JSON.stringify(cmd)));
+
 
 // On login, show message and log
-client.on('ready', () => {
-    console.info(`Ya boi has logged in as ${client.user.tag}`);
+client.login(discord_token);
+
+client.on("ready", () => {
+    console.info(`<SETUP> Logged in as ${client.user.tag}`);
     client.user.setPresence({
-        status: 'online',
+        status: "online",
         game: {
-            name: 'development',
-            type: 'WATCHING'
+            name: "development",
+            type: "WATCHING"
         }
     });
 });
 
-client.on('message', async message => {
-    const prefix = 'm!'
+client.on("message", async message => {
+    const message_author = message.author.username;
+    const server_name = message.guild.name;
 
-    if (message.content.startsWith(prefix)) {
-        // Remove prefix, then split into tokens using space as delimiter
-        const args = message.content.slice(prefix.length).trim().split();
+    // If the server isn't in data, add it
+    if (!client.server_data.hasOwnProperty(server_name)) {
+        client.server_data[server_name] = {
+            "prefix": "m!",
+            "admins": [],
+            "disabled_commands": [],
+            "aliases": [],
+            "queue": [],
+        };
         
-        // Grab command (first token) and convert to lowercase
-        const cmd = args.shift().toLowerCase();
-        
-        let commandToRun = client.commands.get(cmd);
+        console.log(`<DATA> Server data update -> ${JSON.stringify(client.server_data[server_name])}`);
+    }
 
-        if (commandToRun) {
-            commandToRun.run(client, message, args);
-            console.log(`<COMMAND> ${commandToRun.name} ran by ${message.author.username}`);
+    // Now that server data is loaded, do other checks
+    const data = client.server_data[server_name]
+    const prefix = data["prefix"]
+
+    if (!message.content.includes(prefix)) return;
+    if (message.content.length === 0) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // If user isn't in data, add it
+    if (!client.user_data.hasOwnProperty(message_author)) {
+        client.user_data[message_author] = {
+            "playlists": [],
+            "favorite_songs": [],
+            "favorite_playlists": [],
         }
+        console.log(`<DATA> User data update -> ${message_author + ": " + JSON.stringify(client.user_data[message_author])}`);
+    }
+
+    // Message parsing, running commands
+    let args = message.content.slice(prefix.length).trim().split(" ");
+    const cmd = args.shift().toLowerCase();
+    
+    const commandToRun = client.commands.get(cmd);
+
+    if (commandToRun) {
+        commandToRun.run(client, message, args);
+        console.log(`<COMMAND> ${commandToRun.name} ran by ${message.author.username}`);
     }
 });
